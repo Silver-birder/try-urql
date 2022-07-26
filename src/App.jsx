95141626456424ef98ade3ecad8de5f1e7260355
shibamoto <silverbirder@gmail.com>
2022-07-26 15:11:06 +0900

    :construction: Check urql client operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import { pipe, filter, merge, share, map } from "wonka";
-import { createClient, Provider, dedupExchange, fetchExchange } from "urql";
+import { pipe, tap } from "wonka";
+import {
+  createClient,
+  Provider,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+  debugExchange,
+} from "urql";
 
 import PokemonList from "./PokemonList";
 
-const noopExchange = ({ client, forward }) => {
+const customExchange = ({ forward }) => {
   return (operations$) => {
-    const shared = pipe(operations$, share);
-    const queries = pipe(
-      shared,
-      filter((op) => op.kind === "query"),
-      map((op) => {
-        console.log("map", { op });
-        return op;
-      })
+    return forward(
+      pipe(
+        operations$,
+        tap((op) => {
+          console.log({ op });
+          return op;
+        })
+      )
     );
-    const others = pipe(
-      shared,
-      filter((op) => op.kind !== "query")
-    );
-    return forward(merge([queries, others]));
   };
 };
 
 const client = createClient({
   url: "https://trygql.formidable.dev/graphql/basic-pokedex",
-  exchanges: [dedupExchange, noopExchange, fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange,
+    fetchExchange,
+    debugExchange,
+    customExchange,
+  ],
 });
 
 function App() {
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { gql, useQuery } from 'urql';
+import React from "react";
+import { gql, useQuery } from "urql";
 
 const POKEMONS_QUERY = gql`
   query Pokemons {
@@ -14,6 +14,7 @@ const PokemonList = () => {
   const [result] = useQuery({ query: POKEMONS_QUERY });
 
   const { data, fetching, error } = result;
+  console.log({ data });
 
   return (
     <div>
@@ -23,7 +24,7 @@ const PokemonList = () => {
 
       {data && (
         <ul>
-          {data.pokemons.map(pokemon => (
+          {data.pokemons.map((pokemon) => (
             <li key={pokemon.id}>{pokemon.name}</li>
           ))}
         </ul>
