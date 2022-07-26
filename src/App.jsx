cbee0d506d4f7a5ee140e5d293d6d5011a9ff045
shibamoto <silverbirder@gmail.com>
2022-07-26 16:04:04 +0900

    :construction: Found data convert part, but it's dirty

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { pipe, tap } from "wonka";
+import { pipe, share, make, mergeMap, merge } from "wonka";
 import {
   createClient,
   Provider,
@@ -7,21 +7,116 @@ import {
   cacheExchange,
   fetchExchange,
   debugExchange,
+  makeResult,
+  makeErrorResult,
 } from "urql";
+import { print } from "graphql";
 
 import PokemonList from "./PokemonList";
 
-const customExchange = ({ forward }) => {
-  return (operations$) => {
-    return forward(
-      pipe(
-        operations$,
-        tap((op) => {
-          console.log({ op });
-          return op;
-        })
-      )
+// @see: https://github.com/FormidableLabs/urql/issues/560
+const executeFetch = (operation, opts) => {
+  const { url, fetch: fetcher } = operation.context;
+  let response;
+
+  return (fetcher || fetch)(url, opts)
+    .then((res) => {
+      const { status } = res;
+      const statusRangeEnd = opts.redirect === "manual" ? 400 : 300;
+      response = res;
+
+      if (status < 200 || status >= statusRangeEnd) {
+        throw new Error(res.statusText);
+      } else {
+        return res.json();
+      }
+    })
+    .then((result) => {
+      // convert response data to ui logic data.
+      return makeResult(operation, result, response);
+    })
+    .catch((err) => {
+      if (err.name !== "AbortError") {
+        return makeErrorResult(operation, err, response);
+      }
+    });
+};
+
+const createFetchSource = (operation) => {
+  if (
+    process.env.NODE_ENV !== "production" &&
+    operation.operationName === "subscription"
+  ) {
+    throw new Error(
+      `Received a subscription operation in the httpExchange.
+      You are probably trying to create a subscription. Have you added a subscriptionExchange?`
     );
+  }
+
+  return make(({ next, complete }) => {
+    const abortController =
+      typeof AbortController !== "undefined"
+        ? new AbortController()
+        : undefined;
+
+    const { context } = operation;
+
+    const extraOptions =
+      typeof context.fetchOptions === "function"
+        ? context.fetchOptions()
+        : context.fetchOptions || {};
+
+    let ended = false;
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {},
+      ...extraOptions,
+      signal:
+        abortController !== undefined ? abortController.signal : undefined,
+    };
+    const body = {
+      query: print(operation.query),
+      variables: operation.variables,
+    };
+    fetchOptions.body = JSON.stringify(body);
+    fetchOptions.headers["content-type"] = "application/json";
+
+    Promise.resolve()
+      .then(() => (ended ? undefined : executeFetch(operation, fetchOptions)))
+      .then((result) => {
+        if (!ended) {
+          ended = true;
+          if (result) next(result);
+          complete();
+        }
+      });
+    return () => {
+      ended = true;
+      if (abortController !== undefined) {
+        abortController.abort();
+      }
+    };
+  });
+};
+
+const customExchange = ({ forward }) => {
+  return (ops$) => {
+    const sharedOps$ = share(ops$);
+    const fetchResults$ = pipe(
+      sharedOps$,
+      mergeMap((operation) => {
+        return pipe(
+          createFetchSource(
+            operation,
+            operation.operationName === "query" &&
+              !!operation.context.preferGetMethod
+          )
+        );
+      })
+    );
+    const forward$ = pipe(sharedOps$, forward);
+    return merge([fetchResults$, forward$]);
   };
 };
 
@@ -34,6 +129,7 @@ const client = createClient({
     debugExchange,
     customExchange,
   ],
+  maskTypename: true, // but, contain __typename ...
 });
 
 function App() {
